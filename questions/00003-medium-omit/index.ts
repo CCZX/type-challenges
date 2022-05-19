6fd1dd0efd130149b679147a1de159af8d53b40d
CCZX <ccdebuging@gmail.com>
2022-05-19 23:11:03 +0800

    Omit

--- questions/00003-medium-omit/index.ts
+++ questions/00003-medium-omit/index.ts
@@ -1,3 +1,13 @@
+export type Exclude<T, U> = T extends U ? never : T
+
 export type MyOmit<T, K extends keyof T> = {
-  
+  [P in Exclude<keyof T, K>]: T[P]
 }
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type TodoPreview = MyOmit<Todo, 'description' | 'title'>
